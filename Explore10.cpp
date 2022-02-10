c5ee4c3aa45c4e3a677cd2e65bb5c11bcce3f391
DERRICKDRANGO <>
2022-02-10 17:46:11 +0000

    Explore10.cpp is working

--- Explore10.cpp
+++ Explore10.cpp
@@ -9,11 +9,17 @@ Broken::Broken() {
     // int does not have a default constructor
     // modify this code so that all elements in values
     // are set to 0.
+    for(int i = 0; i<4; i++){
+      values[i] = 0;
+    }
 }
 Broken::Broken(int initialValues[4]) {
     // Change only this code so that values will hold the
     // values in initialValues
-    values = initialValues;
+    values[0] = initialValues[0];
+    values[1] = initialValues[1];
+    values[2] = initialValues[2];
+    values[3] = initialValues[3];
 }
 
 void PrintBroken(Broken x) {
